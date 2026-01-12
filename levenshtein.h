5439e95c14ce5495098e6ba864566b22cefa7389
MrSadnir <mesharialamodie@gmail.com>
2026-01-12 16:29:18 +0300

    Clean up levenshtein.h and add HTML comment
    Removed unnecessary whitespace and added HTML comment.

--- levenshtein.h
+++ levenshtein.h
@@ -1,8 +1,7 @@
 #ifndef LEVENSHTEIN_H
 #define LEVENSHTEIN_H
-
 int levenshtein(const char *string1, const char *string2,
 	int swap_penalty, int substitution_penalty,
 	int insertion_penalty, int deletion_penalty);
-
 #endif
+<h1>add html</h1>
